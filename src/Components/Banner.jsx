7ec23552ed0b937561d8aad045b2116765902999
Nawinkumar <nawinkumar@Nawins-MacBook-Pro.local>
2025-07-09 18:36:19 +0530

    NK

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,14 +7,26 @@ const Banner = () => {
       <div className="bannerSection">
         <div className="Banner">
           <div className="Banner-text">
-            <h1>a  Digital Craft Studio</h1>
+            <h1>a Digital Craft Studio</h1>
             {/* <h1> Designing Digital DNa</h1> */}
           </div>
           {/* <div className="Banner-model">
           </div> */}
         </div>
         <div className="banner-video">
-          <video src={assets.bannervideo} autoPlay loop/>
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="auto"
+            disablePictureInPicture
+          >
+            <source src={assets.bannervideo} type="video/webm" />
+            <source src={assets.bannervideo} type="video/mp4" />
+            <source src={assets.bannervideo} type="video/ogg" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </>
