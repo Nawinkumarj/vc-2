c15b161729cf827d4ecc238463046ac544dd1b35
Nawinkumar <nawinkumar@Nawins-MacBook-Pro.local>
2025-07-31 13:37:45 +0530

    NK

--- src/Components/CookiesPopup.jsx
+++ src/Components/CookiesPopup.jsx
@@ -37,7 +37,7 @@ const CookiesPopup = () => {
                     Accept all
                 </button>
                 <button onClick={()=> navigate('/cookie-policy')}>
-                    Cookies
+                    Close
                 </button>
             </div>
         </motion.div>
